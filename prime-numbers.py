2668ac800a98a56e63b9365cd38d13fb628ec7f9
Asia <asiaellis5@gmail.com>
2020-07-01 09:27:22 +0100

    added a quicker solution

--- prime-numbers.py
+++ prime-numbers.py
@@ -1,4 +1,5 @@
 from time import time
+import math
 
 def prime_numbers_to_100():
    for val in range(1, 101):
@@ -16,3 +17,20 @@ prime_numbers_to_100()
 t1= time()
 
 print("Time taken:", t1 - t0)
+
+def prime_numbers():
+  for val in range(2, 101):
+    prime = True
+    for i in range(2, int(math.sqrt(val))+1):
+      if(val % i == 0):
+        prime = False
+    if prime:
+        print(val)
+
+
+t0 = time()
+prime_numbers()
+t1= time()
+
+print("Time taken:", t1 - t0)
+
