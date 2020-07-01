c8fc0d7cb4b090d90b4d6b5c3453fa8de221ee1e
Asia <asiaellis5@gmail.com>
2020-07-01 09:42:41 +0100

    added eratosthenes solution

--- prime-numbers.py
+++ prime-numbers.py
@@ -34,3 +34,18 @@ t1= time()
 
 print("Time taken:", t1 - t0)
 
+def eratosthenes(n):
+  sieve = [True] * (n + 1)
+  for p in range(2, n + 1):
+      if (sieve[p]):
+        print(p)
+        for i in range(p, n + 1, p):
+          sieve[i] = False
+
+
+t0 = time()
+eratosthenes(100)
+t1= time()
+
+print("Time taken:", t1 - t0)
+
