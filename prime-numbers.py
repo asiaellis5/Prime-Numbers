7d8acfbe49489c9d89b3f1a64150fcf09f3b744e
Asia <asiaellis5@gmail.com>
2020-07-01 09:45:50 +0100

    moved alternatives to another file

--- prime-numbers.py
+++ prime-numbers.py
@@ -1,5 +1,4 @@
 from time import time
-import math
 
 def prime_numbers_to_100():
    for val in range(1, 101):
@@ -18,34 +17,5 @@ t1= time()
 
 print("Time taken:", t1 - t0)
 
-def prime_numbers():
-  for val in range(2, 101):
-    prime = True
-    for i in range(2, int(math.sqrt(val))+1):
-      if(val % i == 0):
-        prime = False
-    if prime:
-        print(val)
 
-
-t0 = time()
-prime_numbers()
-t1= time()
-
-print("Time taken:", t1 - t0)
-
-def eratosthenes(n):
-  sieve = [True] * (n + 1)
-  for p in range(2, n + 1):
-      if (sieve[p]):
-        print(p)
-        for i in range(p, n + 1, p):
-          sieve[i] = False
-
-
-t0 = time()
-eratosthenes(100)
-t1= time()
-
-print("Time taken:", t1 - t0)
 
